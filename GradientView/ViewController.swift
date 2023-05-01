f3c84348d4af9180b01a22ba751a1855d4b887c9
Yernar Masujima <Yernar.Massujima@kaspi.kz>
2023-05-01 15:27:03 +0600

    Initial Commit

---
+++ GradientView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientView
+//
+//  Created by Yernar Masujima on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
