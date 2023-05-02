75b9897420d5245e434bdeffea46b4ebb6d27f17
Yernar Masujima <Yernar.Massujima@kaspi.kz>
2023-05-02 22:32:19 +0600

    add gradient view with shadow

--- GradientView/ViewController.swift
+++ GradientView/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+	
+	private let gradientLayer = CAGradientLayer()
+	private let gradientView = UIView()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		view.backgroundColor = .white
+		
+		setupGradientLayer()
+		setupGradientView()
 	}
-
-
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		gradientLayer.frame = gradientView.bounds
+	}
+	
+	private func setupGradientLayer() {
+		gradientLayer.colors = [UIColor.systemMint.cgColor, UIColor.systemOrange.cgColor]
+		gradientLayer.cornerRadius = 13
+		gradientLayer.startPoint = .init(x: 0.0, y: 0.0)
+		gradientLayer.endPoint = .init(x: 1.0, y: 1.0)
+	}
+	
+	private func setupGradientView() {
+		gradientView.layer.shadowColor = UIColor.black.cgColor
+		gradientView.layer.shadowOpacity = 0.3
+		gradientView.layer.shadowOffset = CGSize(width: 0.0, height: 8.0)
+		gradientView.layer.shadowRadius = 10
+		gradientView.layer.insertSublayer(gradientLayer, at: 0)
+		gradientView.translatesAutoresizingMaskIntoConstraints = false
+		
+		view.addSubview(gradientView)
+		NSLayoutConstraint.activate([
+			gradientView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+			gradientView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 100),
+			gradientView.widthAnchor.constraint(equalToConstant: 100),
+			gradientView.heightAnchor.constraint(equalToConstant: 100)
+		])
+	}
 }
-
